9b9bc04c44c5386cffe8fa99124968248bd27e5e
annima24 <nm.andrew24@gmail.com>
2021-03-31 13:23:35 -0400

    set up the calculator in html and then style-wise, has allthe buttons it needs and im going to start working on javascript

--- script.js
+++ script.js
@@ -19,3 +19,8 @@ function divide(a,b)  {
     return a / b;
 }
 
+//Create a new function operate() that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function operate(operator, a, b)  {
+    return operator(a,b);
+}
